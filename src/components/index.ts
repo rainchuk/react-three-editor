5d34f5ee56ccdf93d6e46e5f9f21fc60261fa01b
Rainchuk Vitaliy <rainchuk.vitaliy@gmail.com>
2020-02-28 02:02:21 +0200

    Components index

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+export * from "./Controls/Controls";
+export * from "./ObjectControls/ObjectControls";
+
+/** UI Components */
+export * from "./MenuBar/MenuBar";
+
+/** Models */
+export * from "./Plane/Plane";
+export * from "./Box/Box";
+export * from "./Sphere/Sphere";
+
+/** Properties */
+export * from "./ObjectPropsPanel/ObjectPropsPanel";
