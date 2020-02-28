441d99c785e360341ec580e9688a880482a39ac1
Rainchuk Vitaliy <rainchuk.vitaliy@gmail.com>
2020-02-28 02:01:16 +0200

    Added vars to d.ts

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="react-scripts" />
+declare module JSX {
+  interface IntrinsicElements {
+    group: any;
+    geometry: any;
+    lineBasicMaterial: any;
+    mesh: any;
+    octahedronGeometry: any;
+    meshBasicMaterial: any;
+    orbitControls: any;
+    primitive: any;
+    ambientLight: any;
+    fog: any;
+    planeBufferGeometry: any;
+    material: any;
+  }
+}
